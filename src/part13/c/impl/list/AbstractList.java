bc4640fc8227afe6cfb87206b0ba63dd87900c58
Jung Seohyun <125573226+tjgus027@users.noreply.github.com>
2023-07-25 23:40:35 +0900

    13장 인터페이스와 추상클래스, enum

---
+++ src/part13/c/impl/list/AbstractList.java
@@ -0,0 +1,24 @@
+package part13.c.impl.list;
+
+public abstract class AbstractList implements List{
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void update(int index, Object value) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void search(int index) {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void remove(int index) {
+		// TODO Auto-generated method stub
+	}
+	public abstract void clear();
+}
---
+++ src/part13/c/impl/list/List.java
@@ -0,0 +1,8 @@
+package part13.c.impl.list;
+
+public interface List {
+	public void add();
+	public void update(int index, Object value);
+	public void search(int index);
+	public void remove(int index);
+}
